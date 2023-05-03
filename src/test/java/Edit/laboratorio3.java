ac4eb5db443d3e061eb5d362f2fc7f3dfc119955
Maxi Vidal <maxividali7157@gmail.com>
2023-05-03 10:44:49 -0300

    educacionIt selenium course

--- src/test/java/Edit/laboratorio3.java
+++ src/test/java/Edit/laboratorio3.java
@@ -10,6 +10,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,25 +20,23 @@ import java.time.Duration;
 public class laboratorio3 {
     WebDriver driver;
     String url = "http://automationpractice.pl/index.php";
-    WebElement element;
     File pantalla;
-    String dirEvidencias= "‪E:\\OneDrive\\Desktop\\java proyectos\\dataStuff";
+    String dirEvidencias= "..\\EducacionIt";
 
 
     @BeforeMethod
-    public void laboratorio3() {
+    public void setting() {
         System.setProperty("webdriver.chrome.driver", "C:\\testing\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(url);
         driver.manage().window().maximize();
-        driver.findElement(By.partialLinkText("Sign")).click();
     }
     @Test
     public void contactUs(){
 
         driver.findElement(By.linkText("Contact us")).click();
         Select selSubjet  = new Select (driver.findElement(By.id("id_contact")));
-        String tituloEsperado = "Contact i - My Store";
+        String tituloEsperado = "Contact us - My Store";
         String actualTittle =  driver.getTitle();
         Assert.assertEquals(tituloEsperado,actualTittle);
 
@@ -44,14 +44,20 @@ public class laboratorio3 {
         selSubjet.selectByVisibleText("Customer service");
 
         driver.findElement(By.name("from")).sendKeys("correo@gmail.com");
-        driver.findElement (By.cssSelector("#fileupload")).sendKeys("E:\\OneDrive\\Desktop\\curriculum vitae MVdev.pdf");
+        driver.findElement (By.cssSelector("#fileUpload")).sendKeys(
+                "E:\\OneDrive\\Desktop\\curriculum vitae MVdev.pdf");
+        driver.findElement(By.xpath("//input[@id='id_order']")).sendKeys("123ABC");
+        driver.findElement(By.tagName("textarea")).sendKeys("Mensaje de Contacto a la Empresa");
         driver.findElement(By.id("submitMessage")).click();
+
         WebElement lblMensaje = null;
         try {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'Your message has been successfully sent to our tea')]")));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
+                    ("//p[contains(text(),'Your message has been successfully sent to our tea')]")));
 
-            lblMensaje = driver.findElement(By.xpath("//p[contains(text(),'Your message has been successfully sent to our tea')]"));
+            lblMensaje = driver.findElement(By.xpath
+                    ("//p[contains(text(),'Your message has been successfully sent to our tea')]"));
         } catch (Exception e) {e.printStackTrace();}
         Assert.assertNotNull(lblMensaje);
 
@@ -85,8 +91,5 @@ public class laboratorio3 {
         // Se ejecuta después de terminar todos los @Test
         driver.close();
     }
-
-
-
 
 }
